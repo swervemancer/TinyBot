5ebfbccb631d6b663f23727dde759a3379fc80eb
Adan Silva <ev3mcdesigner@gmail.com>
2022-01-11 16:59:03 -0800

    Merge Bang-Bang into main code

---
+++ src/main/include/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <units/length.h>
+
+namespace Constants {
+    constexpr double kCountsPerRevolution = 1440.0;
+    constexpr double kWheelDiameterInch = 2.75;
+    static constexpr units::meter_t kWheelDiameter = 70_mm;
+}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -8,6 +8,7 @@
 
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
+#include "AutonomousChooser.hpp"
 
 class Robot : public frc::TimedRobot {
  public:
@@ -23,8 +24,5 @@ class Robot : public frc::TimedRobot {
   void TestPeriodic() override;
 
  private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+  
 };
